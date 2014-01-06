33ebb251d1671dbf47e5c2ecd22f45e4ce772b76
sandves <stian@sandve.org>
2014-01-06 20:48:04 +0100

    Initial commit

---
+++ JoystickTester/JoystickView.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace JoystickTester
+{
+    partial class JoystickView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblJoystick = new System.Windows.Forms.Label();
+            this.tmrUpdateStick = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblJoystick
+            // 
+            this.lblJoystick.AutoSize = true;
+            this.lblJoystick.Location = new System.Drawing.Point(12, 9);
+            this.lblJoystick.Name = "lblJoystick";
+            this.lblJoystick.Size = new System.Drawing.Size(45, 13);
+            this.lblJoystick.TabIndex = 0;
+            this.lblJoystick.Text = "Joystick";
+            // 
+            // tmrUpdateStick
+            // 
+            this.tmrUpdateStick.Tick += new System.EventHandler(this.tmrUpdateStick_Tick);
+            // 
+            // JoystickView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.lblJoystick);
+            this.Name = "JoystickView";
+            this.Text = "Joystick Tester";
+            this.Load += new System.EventHandler(this.JoystickView_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblJoystick;
+        private System.Windows.Forms.Timer tmrUpdateStick;
+    }
+}
+
---
+++ JoystickTester/JoystickView.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.DirectX.DirectInput;
+using System.Diagnostics;
+
+namespace JoystickTester
+{
+    public partial class JoystickView : Form
+    {
+        private Device joystick;
+        
+        public JoystickView()
+        {
+            InitializeComponent();
+        }
+
+        private void JoystickView_Load(object sender, EventArgs e)
+        {
+            Debug.WriteLine("load");
+            //create joystick device.
+            foreach (
+                DeviceInstance di in
+                Manager.GetDevices(
+                    DeviceClass.GameControl,
+                    EnumDevicesFlags.AttachedOnly))
+            {
+                joystick = new Device(di.InstanceGuid);
+                break;
+            }
+
+            if (joystick == null)
+            {
+                //Throw exception if joystick not found.
+                throw new Exception("No joystick found.");
+            }
+
+
+            //Set joystick axis ranges.
+            foreach (DeviceObjectInstance doi in joystick.Objects)
+            {
+                if ((doi.ObjectId & (int)DeviceObjectTypeFlags.Axis) != 0)
+                {
+                    joystick.Properties.SetRange(
+                        ParameterHow.ById,
+                        doi.ObjectId,
+                        new InputRange(-100, 100));
+                }
+            }
+
+            //Set joystick axis mode absolute.
+            joystick.Properties.AxisModeAbsolute = true;
+
+
+            joystick.SetCooperativeLevel(
+                this.Handle,
+                CooperativeLevelFlags.NonExclusive |
+                CooperativeLevelFlags.Background);
+
+            //Acquire devices for capturing.
+            joystick.Acquire();
+
+            tmrUpdateStick.Enabled = true;
+        }
+
+        private void UpdateJoystick()
+        {
+            string info = "Joystick: \n";
+
+            //Get Mouse State.
+            JoystickState state = joystick.CurrentJoystickState;
+
+            //Capture stick Position.
+            info += "\nRoll: " + state.X;
+            info += "\nPitch: " + state.Y;
+            info += "\nYaw: " + state.Rz;
+
+            //Capture throttle
+            int throttle = state.GetSlider()[0];
+            int throttlePercentage = (throttle - 100) / -2;
+            info += "\nThrottle: " + throttlePercentage;
+
+            //Capture point-of-view hat
+            int pov = state.GetPointOfView()[0];
+
+            //Convert to degrees if hat is not in center
+            if (pov != -1)
+                pov /= 100;
+
+            //hat is in center
+            if (pov == -1) 
+                info += "\nPoint-of-view hat: center";
+            else
+                info += "\nPoint-of-view hat: " + pov + " degrees";
+
+            //Capture Buttons.
+            byte[] buttons = state.GetButtons();
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                if (buttons[i] != 0)
+                {
+                    info += "\nButton: " + (i + 1);
+                }
+            }
+
+            lblJoystick.Text = info;
+        }
+
+        private void tmrUpdateStick_Tick(object sender, EventArgs e)
+        {
+            UpdateJoystick();
+        }
+    }
+}
