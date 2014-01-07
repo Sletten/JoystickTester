e1802db7eabd6ae71dee8d56c0ad503c08f45105
sandves <stian@sandve.org>
2014-01-07 13:52:57 +0100

    Gauge for PoV hat

---
+++ Joystick/Joystick.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.DirectX.DirectInput;
+
+namespace JoystickInterface
+{
+    public class Joystick
+    {
+        private Device joystick;
+        private JoystickState state;
+
+        public Joystick(IntPtr windowHandle) {
+            createJoystick();
+            configureJoystick(windowHandle);
+        }
+
+        public void UpdateState()
+        {
+            state = joystick.CurrentJoystickState;
+        }
+
+        public JoystickState State()
+        {
+            return joystick.CurrentJoystickState;
+        }
+
+        public int Roll()
+        {
+            return state.X;
+        }
+
+        public int Pitch()
+        {
+            return state.Y;
+        }
+
+        public int Yaw()
+        {
+            return state.Rz;
+        }
+
+        public void Acquire()
+        {
+            //Acquire device for capturing.
+            joystick.Acquire();
+        }
+
+        public void Unacquire()
+        {
+            //Unacquire device for capturing.
+            joystick.Unacquire();
+        }
+
+        public int Throttle()
+        {
+            //Capture throttle
+            int throttle = state.GetSlider()[0];
+            //Compensate for the default throttle range which is -100 to 100
+            //We want it to go from 0 to 100
+            int throttlePercentage = (throttle - 100) / -2;
+            return throttlePercentage;
+        }
+
+        public int PointOfView()
+        {
+            //Capture point-of-view hat
+            int pov = state.GetPointOfView()[0];
+
+            //Convert to degrees if hat is not in center
+            if (pov != -1)
+                pov /= 100;
+
+            return pov;
+        }
+
+        public byte[] Buttons()
+        {
+            //Capture Buttons.
+            byte[] buttons = state.GetButtons();
+            return buttons;
+        }
+
+        private void createJoystick() {
+            //create joystick device.
+            foreach (
+                DeviceInstance di in
+                Manager.GetDevices(
+                    DeviceClass.GameControl,
+                    EnumDevicesFlags.AttachedOnly))
+            {
+                joystick = new Device(di.InstanceGuid);
+                break;
+            }
+
+            if (joystick == null)
+            {
+                //Throw exception if joystick not found.
+                throw new Exception("No joystick found.");
+            }
+
+        }
+
+        private void configureJoystick(IntPtr windowHandle)
+        {
+            //Set joystick axis ranges.
+            foreach (DeviceObjectInstance doi in joystick.Objects)
+            {
+                if ((doi.ObjectId & (int)DeviceObjectTypeFlags.Axis) != 0)
+                {
+                    joystick.Properties.SetRange(
+                        ParameterHow.ById,
+                        doi.ObjectId,
+                        new InputRange(-100, 100));
+                }
+            }
+
+            //Set joystick axis mode absolute.
+            joystick.Properties.AxisModeAbsolute = true;
+
+
+            joystick.SetCooperativeLevel(
+                windowHandle,
+                CooperativeLevelFlags.NonExclusive |
+                CooperativeLevelFlags.Background);
+        }
+    }
+}
--- JoystickTester/JoystickView.cs
+++ JoystickTester/JoystickView.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.DirectX.DirectInput;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,17 +8,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Microsoft.DirectX.DirectInput;
-using System.Diagnostics;
+using JoystickInterface;
+
 
 namespace JoystickTester
 {
     public partial class JoystickView : Form
     {
-        private Device joystick;
+        private Joystick joystick;
 
         private int roll = 0;
         private int pitch = 0;
+        private int pov = -1;
         
         public JoystickView()
         {
@@ -26,47 +28,7 @@ namespace JoystickTester
 
         private void JoystickView_Load(object sender, EventArgs e)
         {
-            Debug.WriteLine("load");
-            //create joystick device.
-            foreach (
-                DeviceInstance di in
-                Manager.GetDevices(
-                    DeviceClass.GameControl,
-                    EnumDevicesFlags.AttachedOnly))
-            {
-                joystick = new Device(di.InstanceGuid);
-                break;
-            }
-
-            if (joystick == null)
-            {
-                //Throw exception if joystick not found.
-                throw new Exception("No joystick found.");
-            }
-
-
-            //Set joystick axis ranges.
-            foreach (DeviceObjectInstance doi in joystick.Objects)
-            {
-                if ((doi.ObjectId & (int)DeviceObjectTypeFlags.Axis) != 0)
-                {
-                    joystick.Properties.SetRange(
-                        ParameterHow.ById,
-                        doi.ObjectId,
-                        new InputRange(-100, 100));
-                }
-            }
-
-            //Set joystick axis mode absolute.
-            joystick.Properties.AxisModeAbsolute = true;
-
-
-            joystick.SetCooperativeLevel(
-                this.Handle,
-                CooperativeLevelFlags.NonExclusive |
-                CooperativeLevelFlags.Background);
-
-            //Acquire devices for capturing.
+            joystick = new Joystick(this.Handle);
             joystick.Acquire();
 
             tmrUpdateStick.Enabled = true;
@@ -74,83 +36,95 @@ namespace JoystickTester
 
         private void UpdateJoystick()
         {
-            string info = "Joystick: \n";
 
-            //Get Mouse State.
-            JoystickState state = joystick.CurrentJoystickState;
-            roll = state.X;
-            pitch = state.Y;
-
+            joystick.UpdateState();
+     
             //Capture stick Position.
-            info += "\nRoll: " + state.X;
-            info += "\nPitch: " + state.Y;
-            info += "\nYaw: " + state.Rz;
-
-            //Capture throttle
-            int throttle = state.GetSlider()[0];
-            //Compensate for the default throttle range which is -100 to 100
-            //We want it to go from 0 to 100
-            int throttlePercentage = (throttle - 100) / -2;
-            info += "\nThrottle: " + throttlePercentage;
+            roll = joystick.Roll();
+            pitch = joystick.Pitch();
 
             //Capture point-of-view hat
-            int pov = state.GetPointOfView()[0];
-
-            //Convert to degrees if hat is not in center
-            if (pov != -1)
-                pov /= 100;
-
-            //hat is in center
-            if (pov == -1) 
-                info += "\nPoint-of-view hat: center";
-            else
-                info += "\nPoint-of-view hat: " + pov + " degrees";
+            pov = joystick.PointOfView();
 
             //Capture Buttons.
-            byte[] buttons = state.GetButtons();
+            byte[] buttons = joystick.Buttons();
             Boolean buttonPushed = false;
             for (int i = 0; i < buttons.Length; i++)
             {
                 if (buttons[i] != 0)
                 {
                     buttonPushed = true;
-                    info += "\nButton: " + (i + 1);
+                    //button i+1 pressed
                 }
             }
 
-            //displat this if no button is pushed
+            //display this if no button is pushed
             if (!buttonPushed)
             {
-                info += "\nButton: none";
+                //none
             }
 
             //Update throttle tracker
-            trkThrottle.Value = throttlePercentage;
+            int throttlePercentage = joystick.Throttle();
+            if (throttlePercentage >= 0 && throttlePercentage <= 100)
+                trkThrottle.Value = throttlePercentage;
 
             //Update yaw tracker
-            trkYaw.Value = state.Rz;
+            int yaw = joystick.Yaw();
+            if ( yaw >= -100 && yaw <= 100)
+                trkYaw.Value = yaw;
 
             this.Invalidate();
         }
 
         protected override void OnPaint(PaintEventArgs pe)
         {
+            //Paint the base before we do anything else
             base.OnPaint(pe);
 
             // get the graphics object to use to draw
             Graphics g = pe.Graphics;
 
+            //Create something we can draw with
             Pen pen = new Pen(Color.Black);
-            Brush brush = new SolidBrush(Color.White);
+            Brush whiteBrush = new SolidBrush(Color.White);
+            Brush blackBrush = new SolidBrush(Color.Black);
 
-            g.FillRectangle(brush, 10, 10, 210, 210);
+            //Joystick position
+            g.FillRectangle(whiteBrush, 10, 10, 210, 210);
             g.DrawRectangle(pen, 10, 10, 210, 210);
-            g.DrawRectangle(pen, 110+roll, 110+pitch, 10, 10);
+            g.FillRectangle(blackBrush, 110+roll, 110+pitch, 10, 10);
+
+            //Circles of PoV
+            for (int i = 0; i < 8; i++)
+            {
+                int x = (int)(50 * Math.Cos(2 * Math.PI * i / 8));
+                int y = (int)(50 * Math.Sin(2 * Math.PI * i / 8));
+                g.DrawEllipse(pen, x + 400, y + 100, 20, 20);
+            }
+
+            //Fill the correct circle when PoV hat is not in center
+            if (pov != -1)
+            {
+                int xPressed = (int)(50 * Math.Cos(2 * Math.PI * (pov/45-2) / 8));
+                int yPressed = (int)(50 * Math.Sin(2 * Math.PI * (pov/45-2) / 8));
+                g.FillEllipse(blackBrush, xPressed + 400, yPressed + 100, 20, 20);
+            }
+            else
+            {
+                //Fill center if PoV hat is not used
+                g.FillEllipse(blackBrush, 400, 100, 20, 20);
+            }
         }
 
         private void tmrUpdateStick_Tick(object sender, EventArgs e)
         {
             UpdateJoystick();
+        }
+
+        private void JoystickView_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            joystick.Unacquire();
         }
     }
 }
